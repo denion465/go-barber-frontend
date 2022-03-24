312b034abb0b896543f97392f700bd4efc6f5e1d
Daniel Vidal <danielsv465@hotmail.com>
2022-03-23 23:26:56 -0300

    Toast messages

---
+++ src/hooks/toast.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+import ToastContainer from '../components/ToastContainer';
+
+export interface ToastMessage {
+  id: string;
+  type?: 'sucess' | 'error' | 'info';
+  title?: string;
+  description?: string;
+}
+
+interface ToastContextData {
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
+}
+
+const ToastContext = createContext<ToastContextData>({} as ToastContextData);
+
+export const ToastProvider: React.FC = ({ children }) => {
+  const [messages, setMessage] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback(({ type, title, description}: Omit<ToastMessage, 'id'>) => {
+    const toast = {
+      id: uuid(),
+      type,
+      title,
+      description
+    };
+
+    setMessage(state => [...state, toast]);
+  }, []);
+
+  const removeToast = useCallback((id: string) => {
+    setMessage(state => state.filter(message => message.id !== id));
+  }, []);
+
+  return (
+    <ToastContext.Provider value={{ addToast, removeToast }}>
+      {children}
+      <ToastContainer messages={messages}/>
+    </ToastContext.Provider>
+  );
+};
+
+export function useToast(): ToastContextData {
+  const context = useContext(ToastContext);
+
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider');
+  }
+
+  return context;
+}
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,7 +9,8 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 interface SignInFormData {
   email: string;
@@ -20,6 +21,7 @@ const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
@@ -35,15 +37,23 @@ const SignIn: React.FC = () => {
         abortEarly: false
       });
 
-      signIn({
+      await signIn({
         email: data.email,
         password: data.password
       });
     } catch (error) {
-      const errors = getValidationErrors(error);
-      formRef.current?.setErrors(errors);
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro na auntenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+      });
     }
-  }, [signIn]);
+  }, [signIn, addToast]);
 
   return (
     <Container>
