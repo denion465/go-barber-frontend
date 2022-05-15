b2b422b5dc48e352b3494afa3db72e021ac818f3
Daniel Vidal <danielsv465@hotmail.com>
2022-05-15 14:04:44 -0300

    Profile page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,6 +27,7 @@ import {
   Appointment,
   Calendar
 } from './styles';
+import { Link } from 'react-router-dom';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -141,7 +142,11 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt="Daniel Vidal" />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link
+                to="/profile"
+              >
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
           <button
